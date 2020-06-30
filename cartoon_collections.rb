711d71a1a3ccb4737507e7046e9281c058e765dd
Brad Beggs <brad.beggs@gmail.com>
2020-06-30 18:40:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,7 @@ def list_dwarves(array)
   # Print a numbered list of each element
 
   array.each_with_index do |elment, index|
-    print "#{index} #{elment}"
+    print "#{index}. *#{elment} "
   end
 
 end
